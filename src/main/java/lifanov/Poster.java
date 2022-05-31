57d28d1d68b52975f2bbac7a680ca14dd9c3a3d8
vallifanov <vallifanov@gmail.com>
2022-05-31 12:35:42 +1200

    исправлено L36

--- src/main/java/lifanov/Poster.java
+++ src/main/java/lifanov/Poster.java
@@ -19,8 +19,7 @@ public class Poster {
         String newFilm = new String("Морбиус");
         int length = films.length + 1;
         String[] tmp = new String[length];
-        for (int i = 0;
-             i < films.length; i++) {
+        for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
         }
         tmp[tmp.length - 1] = newFilm;
@@ -35,16 +34,17 @@ public class Poster {
 
     public String[] findLast(String[] films) {
 
-        int fistMovies = films.length - lastFilms;
+        int fistMovies = films.length;
+        if (fistMovies > lastFilms) {
+            fistMovies = lastFilms;
+        }
 
-        String[] tmp = new String[films.length - fistMovies];
-        int index = 0;
-        for (int i = films.length - 1; i > fistMovies - 1; i = i - 1) {
-            tmp[index] = films[i];
-            index++;
+        String[] tmp = new String[fistMovies];
+        for (int i = 0; i < fistMovies; i++) {
+            int index = fistMovies - i;
+            tmp [i] = films[index];
         }
         return tmp;
     }
-
 }
 
